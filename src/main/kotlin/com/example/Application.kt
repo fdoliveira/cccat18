c974f987e33c041b311dab7e1adc97096de841ac
Francois Oliveira <oliveira.francois@gmail.com>
2024-09-13 19:35:52 -0300

    Create isPassenger attribute to Account Extract code for setInsertStmtParams Add get accounts endpoint

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -12,6 +12,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.postgresql.util.PGobject
 import java.sql.DriverManager
+import java.sql.PreparedStatement
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -19,22 +20,35 @@ fun main(args: Array<String>) {
 
 @Serializable
 data class Account(
+    val accountId: String? = null,
     val name: String,
     val email: String,
     val cpf: String,
+    @SerialName("is_passenger") val isPassenger: Boolean = false,
     @SerialName("is_driver") val isDriver: Boolean = false,
-    @SerialName("car_plate") val carPlate: String?,
+    @SerialName("car_plate") val carPlate: String? = null,
     val password: String
 )
 
 @Serializable
-data class Response(val accountId: String)
+data class Response(@SerialName("account_id") val accountId: String)
 
 fun idToPgObject(uuid: String): PGobject {
     val objectUuid = PGobject()
     objectUuid.setType("uuid")
     objectUuid.setValue(uuid)
     return objectUuid
+}
+
+fun setInsertStmtParams(stmt: PreparedStatement, id: PGobject, account: Account) {
+    stmt.setObject(1, id)
+    stmt.setString(2, account.name)
+    stmt.setString(3, account.email)
+    stmt.setString(4, account.cpf)
+    stmt.setBoolean(5, account.isPassenger)
+    stmt.setBoolean(6, account.isDriver)
+    stmt.setString(7, account.carPlate)
+    stmt.setString(8, account.password)
 }
 
 fun Application.module() {
@@ -47,6 +61,7 @@ fun Application.module() {
                 status = HttpStatusCode.OK
             )
         }
+
         post("/signup") {
             val conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/app", "postgres", "123456")
             val input = call.receive<Account>()
@@ -61,17 +76,11 @@ fun Application.module() {
                     if (input.name.matches(Regex("^[a-zA-Z]+ [a-zA-Z]+$"))) {
                         if (input.email.matches(Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"))) {
                             if (validateCpf(input.cpf)) {
-                                if (input.isDriver && input.carPlate != null) {
+                                if (input.isDriver == true && input.carPlate != null) {
                                     if (input.carPlate.matches(Regex("^[A-Z]{3}[0-9]{4}$"))) {
                                         val stmt =
-                                            conn.prepareStatement("INSERT INTO ccca.account (account_id, name, email, cpf, is_driver, car_plate, password) VALUES (?, ?, ?, ?, ?, ?, ?)")
-                                        stmt.setObject(1, idToPgObject(id))
-                                        stmt.setString(2, input.name)
-                                        stmt.setString(3, input.email)
-                                        stmt.setString(4, input.cpf)
-                                        stmt.setBoolean(5, input.isDriver)
-                                        stmt.setString(6, input.carPlate)
-                                        stmt.setString(7, input.password)
+                                            conn.prepareStatement("INSERT INTO ccca.account (account_id, name, email, cpf, is_passenger, is_driver, car_plate, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
+                                        setInsertStmtParams(stmt, idToPgObject(id), input)
                                         stmt.executeUpdate()
                                         response = Response(id)
                                     } else {
@@ -79,15 +88,8 @@ fun Application.module() {
                                         result = -5
                                     }
                                 } else {
-                                    val stmt =
-                                        conn.prepareStatement("INSERT INTO ccca.account (account_id, name, email, cpf, is_driver, car_plate, password) VALUES (?, ?, ?, ?, ?, ?, ?)")
-                                    stmt.setObject(1, idToPgObject(id))
-                                    stmt.setString(2, input.name)
-                                    stmt.setString(3, input.email)
-                                    stmt.setString(4, input.cpf)
-                                    stmt.setBoolean(5, false)
-                                    stmt.setString(6, null)
-                                    stmt.setString(7, input.password)
+                                    val stmt = conn.prepareStatement("INSERT INTO ccca.account (account_id, name, email, cpf, is_passenger, is_driver, car_plate, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
+                                    setInsertStmtParams(stmt, idToPgObject(id), input)
                                     stmt.executeUpdate()
                                     response = Response(id)
                                 }
@@ -108,11 +110,46 @@ fun Application.module() {
                     result = -4
                 }
                 if (response != null) {
-                    call.respond(response)
+                    call.respond(
+                        message = response,
+                        status = HttpStatusCode.Created
+                    )
                 } else {
                     call.respond(
                         message = result,
                         status = HttpStatusCode.UnprocessableEntity
+                    )
+                }
+            } finally {
+                conn.close()
+            }
+        }
+        get("/accounts/{accountId}") {
+            val conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/app", "postgres", "123456")
+            val accountId = call.parameters["accountId"]
+            try {
+                val statement = conn.prepareStatement("SELECT account_id, name, email, cpf, is_passenger, is_driver, car_plate FROM ccca.account WHERE account_id = ?")
+                statement.setObject(1, idToPgObject(accountId!!))
+                val resultSet = statement.executeQuery()
+                if (resultSet.next()) {
+                    val account = Account(
+                        accountId = resultSet.getObject("account_id").toString(),
+                        name = resultSet.getString("name"),
+                        email = resultSet.getString("email"),
+                        cpf = resultSet.getString("cpf"),
+                        isPassenger = resultSet.getBoolean("is_passenger"),
+                        isDriver = resultSet.getBoolean("is_driver"),
+                        carPlate = resultSet.getString("car_plate"),
+                        password = ""
+                    )
+                    call.respond(
+                        message = account,
+                        status = HttpStatusCode.OK
+                    )
+                } else {
+                    call.respond(
+                        message = "Account not found",
+                        status = HttpStatusCode.NotFound
                     )
                 }
             } finally {
