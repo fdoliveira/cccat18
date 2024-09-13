537768781fd48ebf67c5cd6cad78cd43baa6d1e7
Francois Oliveira <oliveira.francois@gmail.com>
2024-09-13 19:33:24 -0300

    Add ktor client content-negotiation to tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,5 +33,6 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("io.ktor:ktor-client-content-negotiation-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
