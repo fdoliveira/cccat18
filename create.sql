7036c4f6d11bebe561e17f5291d4f9ee915e9d1d
Francois Oliveira <oliveira.francois@gmail.com>
2024-09-15 09:53:13 -0300

    Add table ride

--- create.sql
+++ create.sql
@@ -2,7 +2,7 @@ drop schema if exists ccca cascade;
 
 create schema ccca;
 
-create table ccca.account (
+create table ccca.account(
 	account_id uuid primary key,
 	name text not null,
 	email text not null unique,
@@ -11,4 +11,19 @@ create table ccca.account (
 	is_driver boolean not null default false,
 	car_plate text null,
 	password text not null
+);
+
+create table ccca.ride(
+	ride_id uuid primary key,
+	passenger_id uuid not null,
+    from_lat double precision not null,
+	from_long double precision not null,
+    to_lat double precision not null,
+	to_long double precision not null,
+    status text not null,
+    driver_id uuid null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    foreign key (passenger_id) references ccca.account(account_id),
+    foreign key (driver_id) references ccca.account(account_id)
 );
